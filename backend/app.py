73570cbb27909acdb45e600f08c2835e311041a0
chase-west <chasetwest@gmail.com>
2024-06-25 12:28:19 -0700

    Added ability to send test task to microsoft to do

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_cors import CORS
 from routes import init_routes
+import os
 
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
 CORS(app)
 init_routes(app)
 
--- backend/routes.py
+++ backend/routes.py
@@ -1,7 +1,9 @@
-from flask import request, redirect, url_for
+from flask import Flask, request, redirect, url_for, session, render_template_string
 from requests_oauthlib import OAuth2Session
 import os
 from dotenv import load_dotenv
+import requests
+
 
 load_dotenv()
 
@@ -24,22 +26,71 @@ def init_routes(app):
     def login_microsoft():
         microsoft = OAuth2Session(microsoft_client_id, redirect_uri=microsoft_redirect_uri, scope=["Tasks.ReadWrite"])
         authorization_url, state = microsoft.authorization_url(microsoft_authorization_base_url)
+        session['oauth_state'] = state
         return redirect(authorization_url)
 
     @app.route("/callback/microsoft")
     def callback_microsoft():
-        microsoft = OAuth2Session(microsoft_client_id, redirect_uri=microsoft_redirect_uri)
+        microsoft = OAuth2Session(microsoft_client_id, state=session['oauth_state'], redirect_uri=microsoft_redirect_uri)
         token = microsoft.fetch_token(microsoft_token_url, client_secret=microsoft_client_secret, authorization_response=request.url)
-        return token
+        session['oauth_token'] = token
+        return redirect(url_for('home'))
 
     @app.route("/login/google")
     def login_google():
         google = OAuth2Session(google_client_id, redirect_uri=google_redirect_uri, scope=["https://www.googleapis.com/auth/calendar.readonly"])
         authorization_url, state = google.authorization_url(google_authorization_base_url, access_type="offline", prompt="consent")
+        session['oauth_state'] = state
         return redirect(authorization_url)
 
     @app.route("/callback/google")
     def callback_google():
-        google = OAuth2Session(google_client_id, redirect_uri=google_redirect_uri)
+        google = OAuth2Session(google_client_id, state=session['oauth_state'], redirect_uri=google_redirect_uri)
         token = google.fetch_token(google_token_url, client_secret=google_client_secret, authorization_response=request.url)
-        return token
+        session['oauth_token'] = token
+        return redirect(url_for('home'))
+
+    @app.route("/home")
+    def home():
+        token = session.get('oauth_token')
+        if token:
+            list_id = get_todo_list_id(token['access_token'])
+            task_response = create_tasks(token['access_token'], list_id, "test")
+            return render_template_string("""
+                <h1>Home Page</h1>
+                <p>Task Created:</p>
+                <pre>{{ task_response }}</pre>
+            """, task_response=task_response)
+        else:
+            return "Home Page<br>No valid token found."
+
+def get_todo_list_id(token):
+    headers = {
+        'Authorization': f'Bearer {token}',
+        'Content-Type': 'application/json'
+    }
+    response = requests.get('https://graph.microsoft.com/v1.0/me/todo/lists', headers=headers)
+    lists = response.json().get('value', [])
+    if lists:
+        return lists[0]['id']  # Use the ID of the first To Do list
+    else:
+        raise Exception("No To Do lists found")
+
+def create_tasks(token, list_id, title):
+    headers = {
+        'Authorization': f'Bearer {token}',
+        'Content-Type': 'application/json'
+    }
+    task_data = {
+        'title': title,
+        'dueDateTime': {
+            'dateTime': '2024-06-30T18:00:00.0000000',
+            'timeZone': 'UTC'
+        },
+        'body': {
+            'content': 'This is the body of the task.',
+            'contentType': 'text'
+        }
+    }
+    response = requests.post(f'https://graph.microsoft.com/v1.0/me/todo/lists/{list_id}/tasks', headers=headers, json=task_data)
+    return response.json()

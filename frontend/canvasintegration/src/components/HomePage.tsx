4fd74e8872d8c6774ac1cc00b82f40908952fbca
chase-west <chasetwest@gmail.com>
2024-06-25 13:02:29 -0700

    Update home page

--- frontend/canvasintegration/src/components/HomePage.tsx
+++ frontend/canvasintegration/src/components/HomePage.tsx
@@ -6,7 +6,7 @@ const HomePage: React.FC = () => {
     <div className="jumbotron">
       <div className="container">
         <h1 className="display-4">Welcome to Canvas Integration</h1>
-        <p className="lead">Our website helps you sync your canvas assignments across multiple calender services and to do services.</p>
+        <p className="lead">Our website helps you sync your canvas assignments across multiple calender and to do services.</p>
         <hr className="my-4" />
         <LoginButtons />
       </div>

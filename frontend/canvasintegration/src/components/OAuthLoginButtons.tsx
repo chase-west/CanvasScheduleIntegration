34428d2342792c8237b3822dc01aa1fdf2963ecf
chase-west <chasetwest@gmail.com>
2024-07-01 17:58:22 -0700

    Clarify button names

--- frontend/canvasintegration/src/components/OAuthLoginButtons.tsx
+++ frontend/canvasintegration/src/components/OAuthLoginButtons.tsx
@@ -11,8 +11,8 @@ const OAuthLoginButtons: React.FC = () => {
 
   return (
     <div className="btn-group" role="group" aria-label="Login Buttons">
-      <button type="button" className="btn btn-primary" onClick={loginMicrosoft}>Login with Microsoft</button>
-      <button type="button" className="btn btn-dark" onClick={loginGoogle}>Login with Google</button>
+      <button type="button" className="btn btn-primary" onClick={loginMicrosoft}>Sync assignments with Microsoft</button>
+      <button type="button" className="btn btn-dark" onClick={loginGoogle}>Sync assignments with Google</button>
     </div>
   );
 };
